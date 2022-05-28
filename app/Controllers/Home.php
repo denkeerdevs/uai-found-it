16ff908cdfa084076f5b845cbd3b666f05d20c80
Bagus Dwi <bagus.dwi@if.uai.ac.id>
2022-05-28 08:53:30 +0700

    Tambah method save_barang

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,8 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class Home extends BaseController
 {
+    protected $usersModel;
+    public function __construct()
+    {
+        $this->usersModel = new UsersModel();
+    }
     public function index()
     {
         echo view('templates_user/header');
@@ -15,12 +22,23 @@ class Home extends BaseController
 
     public function form_lapor()
     {
-        //return view('welcome_message');
         echo view('templates_user/header');
-        #echo view('templates/v_sidebar');
-        #echo view('templates/v_topbar');
         echo view('views_user/form_lapor');
         echo view('templates_user/footer');
+    }
+
+    public function save_barang()
+    {
+        //dd($this->request->getVar()); //untuk cek masuk db apa engga
+        //$this->nama model->save(apa yang mau disave pake array)
+        $this->usersModel->save([
+            'nama_barang' => $this->request->getVar('nama_barang'),
+            'kategori_barang' => $this->request->getVar('kategori_barang'),
+            'deskripsi_barang' => $this->request->getVar('deskripsi'),
+            'lokasi_barang' => $this->request->getVar('lokasi')
+        ]);
+
+        return redirect()->to('/home');
     }
 
     public function card_barang()

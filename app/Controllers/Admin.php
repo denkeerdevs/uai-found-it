9664d73ed669017b05ffd759d578d01d005ba282
Bagus Dwi <bagus.dwi@if.uai.ac.id>
2022-05-30 18:23:31 +0700

    penambahan method untuk tampil data

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+
 class Admin extends BaseController
 {
     public function index()
@@ -10,6 +12,28 @@ class Admin extends BaseController
         echo view('templates_admin/sidebar');
         echo view('templates_admin/topbar');
         echo view('views_admin/dashboard');
+        echo view('templates_admin/footer');
+    }
+
+    public function data_masuk()
+    {
+        $data_masuk = $this->usersModel->findAll();
+        $data = [
+            'data_masuk' => $data_masuk
+        ];
+        echo view('templates_admin/header');
+        echo view('templates_admin/sidebar');
+        echo view('templates_admin/topbar');
+        echo view('views_admin/data_masuk', $data);
+        echo view('templates_admin/footer');
+    }
+
+    public function data_keluar()
+    {
+        echo view('templates_admin/header');
+        echo view('templates_admin/sidebar');
+        echo view('templates_admin/topbar');
+        echo view('views_admin/data_keluar');
         echo view('templates_admin/footer');
     }
 }

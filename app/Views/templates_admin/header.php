31c9c945a5fc303aa59cba4eab66f599836d4f05
Bagus Dwi <bagus.dwi@if.uai.ac.id>
2022-05-30 18:34:30 +0700

    edited by Bagus

--- app/Views/templates_admin/header.php
+++ app/Views/templates_admin/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Admin - Dashboard</title>
     <!-- For Icon <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('img/'); ?>favicon/favicon_admin.ico"> -->
 
     <!-- Custom fonts for this template-->
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
--- app/Views/views_user/index.php
+++ app/Views/views_user/index.php
@@ -6,10 +6,13 @@
         <h1 class="display-5 fw-bold">Lost your items??</h1>
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4">Apakah salah satu barang mu terdaftar disini? Segera Lihat Barang Hilang!</p>
-            <div class="d-grid gap-4 d-md-block">
-            <a class="btn btn-info" href="home/card_barang" role="button">Cari Barang Hilang</a>
-                <a class="btn btn-info" href="home/form_lapor" role="button">Lapor Barang Hilang</a>
+            <div class="d-grid gap-2 col-6 mx-auto">
+                <a class="btn btn-info" role="button" href="home/form_lapor">Lapor Barang Hilang</a>
             </div>
+            <!-- <div class="d-grid gap-4 d-md-block">
+                <a class="btn btn-info" href="home/card_barang" role="button">Cari Barang Hilang</a>
+                <a class="btn btn-info" href="home/form_lapor" role="button">Lapor Barang Hilang</a>
+            </div> -->
         </div>
     </div>
 
